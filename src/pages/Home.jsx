c0add50eccbad88ce997cda3880f0385c353c596
Ryan <rphillips0071@gmail.com>
2024-06-07 15:34:47 -0400

    Added functionality to list all recipes and improved search suggestions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 function Home() {
     const [search, setSearch] = useState('');
     const [recipeList, setRecipeList] = useState([]);
+    const [filteredRecipes, setFilteredRecipes] = useState([]);
+    const [suggestions, setSuggestions] = useState([]);
 
     useEffect(() => {
         fetch('https://rphillips0071.github.io/culturalRecipieAPI/full_recipes.json')
@@ -13,8 +15,34 @@ function Home() {
 
     const clickTesting = (e) => {
         e.preventDefault();
-        console.log(search);
+        const filtered = recipeList.filter(country => 
+            country.country.toLowerCase().includes(search.toLowerCase())
+        );
+        setFilteredRecipes(filtered);
         setSearch('');
+    };
+
+    const handleInputChange = (e) => {
+        const value = e.target.value;
+        setSearch(value);
+
+        if (value.length > 0) {
+            const filteredSuggestions = recipeList.filter(country =>
+                country.country.toLowerCase().includes(value.toLowerCase())
+            ).map(country => country.country);
+            setSuggestions(filteredSuggestions);
+        } else {
+            setSuggestions([]);
+        }
+    };
+
+    const handleSuggestionClick = (suggestion) => {
+        setSearch(suggestion);
+        setSuggestions([]);
+    };
+
+    const handleListAllClick = () => {
+        setFilteredRecipes(recipeList);
     };
 
     return (
@@ -28,20 +56,27 @@ function Home() {
                     type="text"
                     placeholder='Search Country'
                     className='input_field'
-                    onChange={(e) => setSearch(e.target.value)}
+                    onChange={handleInputChange}
                     value={search}
                 />
                 <br />
                 <button onClick={clickTesting}>Search</button>
+                <button onClick={handleListAllClick}>List All Recipes</button>
+                <div className='name_suggestions'>
+                    <ul>
+                        {suggestions.map((suggestion, index) => (
+                            <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
+                                {suggestion}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
             <div className='recipe_card_container'>
-
                 <h1>Top Savory Dishes</h1>
                 <div className='savory'>
-
-                    {recipeList.map((country, countryIndex) => (
+                    {filteredRecipes.map((country, countryIndex) => (
                         country.dishes.map((dish, dishIndex) => (
-
                             <div key={dishIndex} className='card'>
                                 <img src={`https://rphillips0071.github.io/culturalRecipieAPI/assets/${dish.name}.jpg`} alt="" />
                                 <h2 className='card_heading'>Country</h2>
@@ -55,18 +90,18 @@ function Home() {
                     ))}
                 </div>
             </div>
-
             <div className='dessert_card_container'>
-                {recipeList.map((country, countryIndex) => (
+                {filteredRecipes.map((country, countryIndex) => (
                     <div key={`dessert-${countryIndex}`}>
                         <h1>Dessert</h1>
                         <div className='dessert'>
                             <div className='dessert_card'>
-                            <img src={`https://rphillips0071.github.io/culturalRecipieAPI/assets/${country.dessert.name}.jpg`} alt="" />
-                                <h2 className='card_heading'>Country</h2> <br /> 
+                                <img src={`https://rphillips0071.github.io/culturalRecipieAPI/assets/${country.dessert.name}.jpg`} alt="" />
+                                <h2 className='card_heading'>Country</h2>
                                 <h3>{country.country}</h3>
-                                <h2 className='card_heading'>Dish </h2><br /> <h3>{country.dessert.name}</h3>
-                                <h2 className='card_heading'>About This Dish </h2>
+                                <h2 className='card_heading'>Dish</h2>
+                                <h3>{country.dessert.name}</h3>
+                                <h2 className='card_heading'>About This Dish</h2>
                                 <p>{country.dessert.description}</p>
                             </div>
                         </div>
